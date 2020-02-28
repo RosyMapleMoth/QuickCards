df1546833d0b1841240cde072e1c34aaa6da163c
Seaney Shell <sfs185@humboldt.edu>
2020-02-27 19:26:43 -0800

    :sparkles: show Answer on click
    answer now shows on click

--- app/src/main/java/com/example/madquickcards/MainActivity.java
+++ app/src/main/java/com/example/madquickcards/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.madquickcards;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvQuestion = findViewById(R.id.tvQuestion);
+        final TextView tvAnswer = findViewById(R.id.tvAnswer);
+
+
+        tvQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvQuestion.setVisibility(View.INVISIBLE);
+                tvAnswer.setVisibility(View.VISIBLE);
+            }
+        });
+        tvAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoadNextCard();
+                Toast.makeText(getApplicationContext(),"Switching card",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
+
+
+
+    public void LoadNextCard()
+    {
+        //TODO
+    }
+
 }
