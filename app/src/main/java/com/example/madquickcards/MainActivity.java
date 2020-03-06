7498d5b846521f13863489e4ffabb39d7472d5ff
Seaney Shell <afc76927@opayq.com>
2020-03-05 21:36:47 -0800

    finished required functionallity

--- app/src/main/java/com/example/madquickcards/MainActivity.java
+++ app/src/main/java/com/example/madquickcards/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -13,11 +14,18 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
 
 
     private boolean showing = true;
+    private FlashcardDatabase flashcardDatabase;
+    private List<Flashcard> allFlashcards;
+    private int currentCardDisplayedIndex = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,67 @@ public class MainActivity extends AppCompatActivity {
         final ImageView ivCompose = findViewById(R.id.ivCompose);
         final ImageView ivEdit = findViewById(R.id.ivEdit);
 
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+
+
+
+        findViewById(R.id.ivNext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex > allFlashcards.size() - 1) {
+                    currentCardDisplayedIndex = 0;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.tvQuestion)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.tvAnswer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            }
+        });
+
+
+        findViewById(R.id.ivPrev).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex--;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if (currentCardDisplayedIndex < 0) {
+                    currentCardDisplayedIndex = allFlashcards.size() - 1;
+                }
+
+                // set the question and answer TextViews with data from the database
+                ((TextView) findViewById(R.id.tvQuestion)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
+                ((TextView) findViewById(R.id.tvAnswer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
+            }
+        });
+
+
+        findViewById(R.id.ivTrash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardDatabase.deleteCard(((TextView) findViewById(R.id.tvQuestion)).getText().toString());
+                allFlashcards = flashcardDatabase.getAllCards();
+Fi
+            }
+        });
+
+
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.tvQuestion)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.tvAnswer)).setText(allFlashcards.get(0).getAnswer());
+            Log.d("MADMain","adding card " + allFlashcards.get(0).getQuestion());
+        }
+        else
+        {
+            Log.d("MADMain","unable to add " + (String) allFlashcards.get(0).getQuestion());
+
+        }
 
 
         tvQuestion.setOnClickListener(new View.OnClickListener() {
@@ -130,6 +199,10 @@ public class MainActivity extends AppCompatActivity {
             ((TextView)findViewById(R.id.tvQuestion)).setText(data.getExtras().getString("question"));
             findViewById(R.id.tvQuestion).setVisibility(View.VISIBLE);// 'string1' needs to match the key we used when we put the string in the Intent
             ((TextView)findViewById(R.id.tvAnswer)).setText(data.getExtras().getString("answer")); // 'string1' needs to match the key we used when we put the string in the Intent
+            findViewById(R.id.tvAnswer).setVisibility(View.INVISIBLE);
+
+
+            // set up multiple choice questions
             ((TextView)findViewById(R.id.tvAOne)).setText(data.getExtras().getString("answer"));
             ((TextView)findViewById(R.id.tvAOne)).setBackgroundResource(R.drawable.card_a_drawable);
             ((TextView)findViewById(R.id.tvAOne2)).setText(data.getExtras().getString("answerWrong"));
@@ -138,8 +211,13 @@ public class MainActivity extends AppCompatActivity {
             ((TextView)findViewById(R.id.tvAOne3)).setBackgroundResource(R.drawable.card_a_drawable);
 
 
+            // add to current database
+            flashcardDatabase.insertCard(new Flashcard(data.getExtras().getString("question"), data.getExtras().getString("answer")));
+            allFlashcards = flashcardDatabase.getAllCards();
 
 
+
+            // Make card made notifaction on bottom of screen
             Snackbar.make(findViewById(R.id.rellay),
                     "Flashcard successfully made!",
                     Snackbar.LENGTH_SHORT)
